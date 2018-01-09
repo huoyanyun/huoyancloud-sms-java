442d82737f5213d7a5958f61756403e846ac7fd8
MichaelY <yanlong1989@gmail.com>
2018-01-09 17:26:49 +0800

    init

---
+++ src/main/java/com/huoyancloud/sms/SmsTest.java
@@ -0,0 +1,14 @@
+package com.huoyancloud.sms;
+
+public class SmsTest {
+
+    /**
+     * 请将参数替换成自己的
+     * templateParam JSON字符串,如"{\"code\":\"1234\"}"
+     */
+    public static void main(String[] args) {
+        SmsClient client = new SmsClient("XXXX", "XXX");
+        String res = client.sendSms("XXXX", "XXXX", "XXXX", "XXXX");
+        System.out.println(res);
+    }
+}
